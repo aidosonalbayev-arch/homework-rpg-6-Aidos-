04e419d4dc2bf329f2131d1c64e09b3b105f04b2
aidosonalbayev-arch <aidos.onalbayev@narxoz.kz>
2026-03-29 22:39:20 +0500

    Implement ArmorHandler with flat damage reduction

--- src/com/narxoz/rpg/chain/ArmorHandler.java
+++ src/com/narxoz/rpg/chain/ArmorHandler.java
@@ -3,6 +3,7 @@ package com.narxoz.rpg.chain;
 import com.narxoz.rpg.arena.ArenaFighter;
 
 public class ArmorHandler extends DefenseHandler {
+
     private final int armorValue;
 
     public ArmorHandler(int armorValue) {
@@ -11,8 +12,9 @@ public class ArmorHandler extends DefenseHandler {
 
     @Override
     public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Subtract armorValue from incomingDamage; clamp the result to a minimum of 0.
-        // TODO: Print an armor message showing how much was absorbed.
-        // TODO: Pass the remaining damage to the next handler.
+        int remainingDamage = Math.max(0, incomingDamage - armorValue);
+        int absorbed = incomingDamage - remainingDamage;
+        System.out.println("[Armor] Absorbed " + absorbed + " damage. Remaining: " + remainingDamage);
+        passToNext(remainingDamage, target);
     }
 }
